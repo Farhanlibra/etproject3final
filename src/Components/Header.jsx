6951128c26dcedcf6139d9f94ba08b5216835296
farhanlibra1984 <farhanlibra1984@outlook.com>
2024-08-06 05:53:38 -0700

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,12 +7,12 @@ const Header = () => {
          <div> 
         <div className="bg-[url('/public/hero.jpg')]  lg:h-[600px]  h-[400px] w-full bg-cover ">
         <div className=' text-white text-xl  py-8'>
-          <Link className='text-[24px] sm: mr-2 sm:ml-1 sm:text-sm lg:mr-[680px] lg:ml-9 font-bold' to='/home'>FARAME</Link>
-          <Link className='text-[18px] mr-5' to='/'>HOME</Link>
-          <Link className='text-[18px] mr-5' to='/feature'>FEATURE</Link>
-          <Link className='text-[18px] mr-5' to='/contact'>CONTACT</Link>
-          <Link className='text-[18px] mr-5' to='/price'>PRICE</Link>
-          <Link className='text-[18px] mr-5' to='/review'>REVIEW</Link>
+          <Link className='text-[24px] sm: mr-2 sm:ml-1 sm:text-[8px] lg:mr-[680px] lg:ml-9 font-bold' to='/home'>FARAME</Link>
+          <Link className='text-[18px] sm:mr-1 lg:mr-5' to='/'>HOME</Link>
+          <Link className='text-[18px] sm:mr-1 lg:mr-5' to='/feature'>FEATURE</Link>
+          <Link className='text-[18px] sm:mr-1 lg:mr-5' to='/contact'>CONTACT</Link>
+          <Link className='text-[18px] sm:mr-1 lg:mr-5' to='/price'>PRICE</Link>
+          <Link className='text-[18px] sm:mr-1 lg:mr-5' to='/review'>REVIEW</Link>
         </div>
         <div className='container'>
         <p className='text-white sm:ml-4 lg:ml-[300px]  lg:w-[600px] text-center mx-auto lg:text-[50px] md:text-[40px] sm:text-[20px] pt-[10px] mt-3 '>uicookies.com Creates High Quality Bootstrap Template For Free</p>
