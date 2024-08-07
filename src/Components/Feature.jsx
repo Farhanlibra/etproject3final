786f7fd91efb297434f4834ec8a1bb9c445b40a6
farhanlibra1984 <farhanlibra1984@outlook.com>
2024-08-07 02:46:53 -0700

    Update Feature.jsx

--- src/Components/Feature.jsx
+++ src/Components/Feature.jsx
@@ -11,7 +11,7 @@ const Feature = () => {
         <div className=' sm: w-[360px] lg:w-[600px] mx-auto my-3 px-2 order-1 lg:order-1'  >
             <img className=' sm:mt-6 ' src='f1.jpg'/>
         </div>
-        <div className=' w-[400px] lg:ml-10 order-2 lg:order-2'>
+        <div className=' w-[400px] ml-10 sm:ml-6  order-2 lg:order-2'>
             <h1 className='text-blue-950 font-bold lg:text-[28px] mb-4  sm:text-[14px] sm:ml-20  lg:ml-16'> Big Benefits for Small Business</h1>
             <p className='lg:ml-16  sm:mt-4 leading-6'>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
             <ul className='mt-8 ml-20 mb-20 list-disc '>
@@ -27,9 +27,9 @@ const Feature = () => {
         <div className='  sm: w-[360px] lg:w-[600px] mx-auto my-2 px-2 order-3 lg:order-4' >
             <img className='sm:mt-6 ' src='f2.jpg'/>
         </div>
-        <div className=' w-[400px] lg:ml-16  sm:ml-6 order-4 lg:order-3'>
+        <div className=' w-[400px] ml-10 sm:ml-6 order-4 lg:order-3'>
         <h1 className='text-blue-950 font-bold lg:text-[28px] mb-4  sm:text-[14px] sm:pl-6 lg:ml-16 '> Big Benefits for Small Business</h1>
-            <p className='lg:ml-16  leading-6'>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+            <p className='lg:ml-16   leading-6'>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
             <ul className='mt-8 ml-20 mb-20 list-disc '>
               <li className='leading-10'>
               Pointing has no control 
